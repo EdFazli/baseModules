4dd1bcc0ba91563f9cb84960f800cc2f44c297db
EdFazli <edfazli92@gmail.com>
2021-10-16 05:56:39 +0800

    updating vpc modules

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,17 +6,17 @@ module "vpc" {
   name                   = each.value.name
   cidr                   = each.value.cidr
 
-  azs                    = lookup(each.value, "azs", null)
+  azs                    = lookup(each.value, "azs", default)
   private_subnets        = each.value.private_subnets
   private_subnets_name   = each.value.private_subnets_name
   public_subnets         = each.value.public_subnets
   public_subnets_name    = each.value.public_subnets_name
 
-  enable_nat_gateway     = lookup(each.value, "enable_nat_gateway", null)
-  one_nat_gateway_per_az = lookup(each.value, "one_nat_gateway_per_az", null)
-  enable_vpn_gateway     = lookup(each.value, "enable_vpn_gateway", null)
+  enable_nat_gateway     = lookup(each.value, "enable_nat_gateway", default)
+  one_nat_gateway_per_az = lookup(each.value, "one_nat_gateway_per_az", default)
+  enable_vpn_gateway     = lookup(each.value, "enable_vpn_gateway", default)
   customer_gateways      = lookup(each.value, "customer_gateways", null)
-  enable_flow_log        = lookup(each.value, "enable_flow_log", null)
+  enable_flow_log        = lookup(each.value, "enable_flow_log", default)
 
   tags                   = lookup(each.value, "tags", null)
 
