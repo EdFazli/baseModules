792d57f56cfecca95401f159b7a59251a6b13585
EdFazli <edfazli92@gmail.com>
2021-10-16 03:41:12 +0800

    updating var vpc_id sg modules

--- modules/security-groups/main.tf
+++ modules/security-groups/main.tf
@@ -15,7 +15,7 @@ module "security-group" {
 
   name                      = lookup(sg.value, "sg_name", null)
   description               = lookup(sg.value, "sg_description", null)
-  vpc_id                    = lookup(sg.value, "sg_vpc_id", default)
+  vpc_id                    = "${data.terraform_remote_state.vpc.vpc_id}"
 
   ingress_with_cidr_blocks  = lookup(sg.value, "sg_ingress_with_cidr_blocks", null)
   egress_with_cidr_blocks   = lookup(sg.value, "sg_egress_with_cidr_blocks", null)
