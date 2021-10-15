3e1fe8212e866b753c7c77b8dd15f23b5df15f72
EdFazli <edfazli92@gmail.com>
2021-10-15 16:18:56 +0800

    updating aurora modules

--- modules/rds/rds-aurora/main.tf
+++ modules/rds/rds-aurora/main.tf
@@ -2,40 +2,42 @@ module "rds-aurora" {
   source  = "terraform-aws-modules/rds-aurora/aws"
   version = "5.2.0"
   
-  name                                = var.aurora_name
-  engine                              = var.aurora_engine
-  engine_version                      = var.aurora_engine_version
-  instance_type                       = var.aurora_instance_type
-  username                            = var.aurora_username
-  password                            = var.aurora_password
-  port                                = var.aurora_port
-  publicly_accessible                 = var.aurora_publicly_accessible
-  backup_retention_period             = var.aurora_backup_retention_period
-  copy_tags_to_snapshot               = var.aurora_copy_tags_to_snapshot
-  auto_minor_version_upgrade          = var.aurora_auto_minor_version_upgrade
-  deletion_protection                 = var.aurora_deletion_protection
+  for_each = var.aurora
 
-  vpc_id                              = var.aurora_vpcid
-  subnets                             = var.aurora_subnets
+  name                                = lookup(aurora.value, "aurora_name", null)
+  engine                              = lookup(aurora.value, "aurora_engine", null)
+  engine_version                      = lookup(aurora.value, "aurora_engine_version", null)
+  instance_type                       = lookup(aurora.value, "aurora_instance_type", null)
+  username                            = lookup(aurora.value, "aurora_username", null)
+  password                            = lookup(aurora.value, "aurora_password", null)
+  port                                = lookup(aurora.value, "aurora_port", null)
+  publicly_accessible                 = lookup(aurora.value, "aurora_publicly_accessible", null)
+  backup_retention_period             = lookup(aurora.value, "aurora_backup_retention_period", null)
+  copy_tags_to_snapshot               = lookup(aurora.value, "aurora_copy_tags_to_snapshot", null)
+  auto_minor_version_upgrade          = lookup(aurora.value, "aurora_auto_minor_version_upgrade", null)
+  deletion_protection                 = lookup(aurora.value, "aurora_deletion_protection", null)
 
-  replica_count                       = var.aurora_replica_count
+  vpc_id                              = lookup(aurora.value, "aurora_vpcid", null)
+  subnets                             = lookup(aurora.value, "aurora_subnets", null)
+
+  replica_count                       = lookup(aurora.value, "aurora_replica_count", null)
 
   create_security_group               = false
-  vpc_security_group_ids              = var.aurora_vpc_security_group_ids
-  allowed_cidr_blocks                 = var.aurora_vpccidr
+  vpc_security_group_ids              = lookup(aurora.value, "aurora_vpc_security_group_ids", null)
+  allowed_cidr_blocks                 = lookup(aurora.value, "aurora_vpccidr", null)
 
-  storage_encrypted                   = var.aurora_storage_encrypted
-  apply_immediately                   = var.aurora_apply_immediately
-  monitoring_interval                 = var.aurora_monitoring_interval
-  iam_role_name                       = var.aurora_iam_role_name
-  iam_database_authentication_enabled = var.aurora_iam_database_authentication_enabled
+  storage_encrypted                   = lookup(aurora.value, "aurora_storage_encrypted", null)
+  apply_immediately                   = lookup(aurora.value, "aurora_apply_immediately", null)
+  monitoring_interval                 = lookup(aurora.value, "aurora_monitoring_interval", null)
+  iam_role_name                       = lookup(aurora.value, "aurora_iam_role_name", null)
+  iam_database_authentication_enabled = lookup(aurora.value, "aurora_iam_database_authentication_enabled", null)
 
-  db_parameter_group_name             = var.aurora_db_parameter_group_name
-  db_cluster_parameter_group_name     = var.aurora_db_cluster_parameter_group_name
+  db_parameter_group_name             = lookup(aurora.value, "aurora_db_parameter_group_name", null)
+  db_cluster_parameter_group_name     = lookup(aurora.value, "aurora_db_cluster_parameter_group_name", null)
 
-  enabled_cloudwatch_logs_exports     = var.aurora_enabled_cloudwatch_logs_exports
-  performance_insights_enabled        = var.aurora_performance_insights_enabled 
+  enabled_cloudwatch_logs_exports     = lookup(aurora.value, "aurora_enabled_cloudwatch_logs_exports", null)
+  performance_insights_enabled        = lookup(aurora.value, "aurora_performance_insights_enabled", null)
 
-  tags                                = var.aurora_tags
+  tags                                = lookup(aurora.value, "aurora_tags", null)
 
 }
